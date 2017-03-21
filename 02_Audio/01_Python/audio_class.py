737e5b9ba64eefa76bb923f7c72bc35ed69bf768
Huy Duc. Vu <huyvu7495@gmail.com>
2017-03-22 00:34:14 +0700

    Update audio module

--- 02_Audio/01_Python/audio_class.py
+++ 02_Audio/01_Python/audio_class.py
@@ -7,8 +7,8 @@ import os
 import os.path
 import subprocess
 # Define class
-#PROJECTPATH = r"/home/pi/healthsystem/"
-PROJECTPATH = r"D:\99_repo\99_local"
+PROJECTPATH = r"/home/pi/healthsystem/"
+#PROJECTPATH = r"D:\99_repo\99_local"
 MP3_SUFFIX = r"mp3"
 MP3PATH = os.path.join(PROJECTPATH, MP3_SUFFIX)
 print MP3PATH
@@ -23,6 +23,7 @@ class MP3:
         self.path = path
         self.fullpath = os.path.join(self.path, self.name)
         self.exist_flag = self.is_exist()
+        self.was_played_flag = 0
         if self.exist_flag==0:
             self.missing()
         return
