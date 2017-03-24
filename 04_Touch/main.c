ad3be70b043f38d6fa741c8887a9d4e2ce5334b5
congly1956 <congly1956@gmail.com>
2017-03-24 19:18:23 +0700

    Create main.c
    update main.c for touch , this version create new file " touch_finish" when  user touch "button start'' , GPIO_Pin_10 set to 1 ...

---
+++ 04_Touch/main.c
@@ -0,0 +1,253 @@
+/**
+  ******************************************************************************
+  * @file    GPIO/IOToggle/main.c 
+  * @author  MCD Application Team
+  * @version V3.5.0
+  * @date    08-April-2011
+  * @brief   Main program body.
+  ******************************************************************************
+  * @attention
+  *
+  * THE PRESENT FIRMWARE WHICH IS FOR GUIDANCE ONLY AIMS AT PROVIDING CUSTOMERS
+  * WITH CODING INFORMATION REGARDING THEIR PRODUCTS IN ORDER FOR THEM TO SAVE
+  * TIME. AS A RESULT, STMICROELECTRONICS SHALL NOT BE HELD LIABLE FOR ANY
+  * DIRECT, INDIRECT OR CONSEQUENTIAL DAMAGES WITH RESPECT TO ANY CLAIMS ARISING
+  * FROM THE CONTENT OF SUCH FIRMWARE AND/OR THE USE MADE BY CUSTOMERS OF THE
+  * CODING INFORMATION CONTAINED HEREIN IN CONNECTION WITH THEIR PRODUCTS.
+  *
+  * <h2><center>&copy; COPYRIGHT 2011 STMicroelectronics</center></h2>
+  ******************************************************************************
+  */ 
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f10x.h"
+#include "main.h"
+#include <stdio.h>
+/** @addtogroup STM32F10x_StdPeriph_Examples
+  * @{
+  */
+
+/** @addtogroup GPIO_IOToggle
+  * @{
+  */
+
+/* Private typedef -----------------------------------------------------------*/
+/* Private define ------------------------------------------------------------*/
+/* Private macro -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+extern Button_DrvTypeDef  *Touch;
+//extern RF_DrvTypeDef  *RF;
+
+__IO uint16_t CCR1_Val = 25000;
+__IO uint16_t CCR2_Val = 10000;
+__IO uint16_t CCR3_Val = 1000;
+__IO uint16_t CCR4_Val = 500;
+
+
+extern Setting_TypeDef Setting;
+extern Data_TypeDef Data;
+uint32_t i;
+
+/* Private function prototypes -----------------------------------------------*/
+/* Private functions ---------------------------------------------------------*/
+void (*Process)(void);
+static void Timer2_Configuration(void);
+static void JTAG_Disable(void);
+static void ClearTouch(void);
+/**
+  * @brief  Main program.
+  * @param  None
+  * @retval None
+  */
+int main(void)
+{
+  if (SysTick_Config(SystemCoreClock / 1000))
+  { 
+    /* Capture error */ 
+    while (1);
+  }
+  JTAG_Disable();
+  RELAY_Init();
+  ZDetect_Init();
+  /* #1##Startup all module */
+  #ifdef __USING_UART_DEBUG
+    UART_DEBUG_Init();  
+  #endif
+  LED_Init();
+	//Buzzer_Init();
+  Touch_Init();
+  Timer2_Configuration();
+	
+  Process =&v_Run;
+
+  while (1)
+  {
+    Touch->InputTask();
+    Process();
+    Zdetect_Task();
+    ClearTouch();		
+  }
+}
+
+/**
+  * @brief : Timer config
+  * @param : none   
+  * @retval : none
+  */
+static void Timer2_Configuration(void)
+{
+	
+  /* ---------------------------------------------------------------
+    TIM2 Configuration: Output Compare Timing Mode:
+    TIM2 counter clock at 100kHz
+    CC1 update rate = TIM2 counter clock / CCR1_Val = 4Hz   250ms
+    CC2 update rate = TIM2 counter clock / CCR2_Val = 10Hz  100ms
+    CC3 update rate = TIM2 counter clock / CCR3_Val = 100Hz 10ms
+    CC4 update rate = TIM2 counter clock / CCR4_Val = 200Hz 5ms
+  --------------------------------------------------------------- */
+
+  /* SystemCoreClock is set to 72 MHz for Low-density, Medium-density, High-density
+    and Connectivity line devices and to 24 MHz for Low-Density Value line and
+    Medium-Density Value line devices
+  --------------------------------------------------------------- */
+  TIM_TimeBaseInitTypeDef  TIM_TimeBaseStructure;
+	TIM_OCInitTypeDef  TIM_OCInitStructure;
+	NVIC_InitTypeDef NVIC_InitStructure;
+	
+	uint16_t PrescalerValue = 0;
+  /* TIM2 clock enable */
+  RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);
+  
+  /* Compute the prescaler value */
+  PrescalerValue = (uint16_t) (SystemCoreClock / (2*100000)) - 1;
+
+  /* Time base configuration */
+  TIM_TimeBaseStructure.TIM_Period = 65535;
+  TIM_TimeBaseStructure.TIM_Prescaler = 0;
+  TIM_TimeBaseStructure.TIM_ClockDivision = 0;
+  TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
+
+  TIM_TimeBaseInit(TIM2, &TIM_TimeBaseStructure);
+
+  /* Prescaler configuration */
+  TIM_PrescalerConfig(TIM2, PrescalerValue, TIM_PSCReloadMode_Immediate);
+
+  /* Output Compare Timing Mode configuration: Channel1 */
+  TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_Timing;
+  TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
+  TIM_OCInitStructure.TIM_Pulse = CCR1_Val;
+  TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_High;
+
+  TIM_OC1Init(TIM2, &TIM_OCInitStructure);
+
+  TIM_OC1PreloadConfig(TIM2, TIM_OCPreload_Disable);
+
+  /* Output Compare Timing Mode configuration: Channel2 */
+  TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
+  TIM_OCInitStructure.TIM_Pulse = CCR2_Val;
+
+  TIM_OC2Init(TIM2, &TIM_OCInitStructure);
+
+  TIM_OC2PreloadConfig(TIM2, TIM_OCPreload_Disable);
+
+  /* Output Compare Timing Mode configuration: Channel3 */
+  TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
+  TIM_OCInitStructure.TIM_Pulse = CCR3_Val;
+
+  TIM_OC3Init(TIM2, &TIM_OCInitStructure);
+
+  TIM_OC3PreloadConfig(TIM2, TIM_OCPreload_Disable);
+
+  /* Output Compare Timing Mode configuration: Channel4 */
+  TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
+  TIM_OCInitStructure.TIM_Pulse = CCR4_Val;
+
+  TIM_OC4Init(TIM2, &TIM_OCInitStructure);
+
+  TIM_OC4PreloadConfig(TIM2, TIM_OCPreload_Disable);
+
+  /* TIM IT enable */
+  TIM_ITConfig(TIM2, TIM_IT_CC1 | TIM_IT_CC2 | TIM_IT_CC3 | TIM_IT_CC4, ENABLE);
+
+  /* TIM2 enable counter */
+  TIM_Cmd(TIM2, ENABLE);
+	
+
+	/* Configure the NVIC Preemption Priority Bits */  
+  NVIC_PriorityGroupConfig(NVIC_PriorityGroup_0);
+	
+  /* Enable the TIM2 global Interrupt */
+  NVIC_InitStructure.NVIC_IRQChannel = TIM2_IRQn;
+  NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
+  NVIC_InitStructure.NVIC_IRQChannelSubPriority = 1;
+  NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+	NVIC_Init(&NVIC_InitStructure);
+}
+
+
+/**
+  * @brief : Disable JTAG pin
+  * @param : none  
+  * @retval : none
+  */
+static void JTAG_Disable(void)
+{
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO, ENABLE);
+
+  /* Disable the Serial Wire Jtag Debug Port SWJ-DP */
+  GPIO_PinRemapConfig(GPIO_Remap_SWJ_JTAGDisable, ENABLE);
+}
+
+
+/**
+  * @brief : Clear all pending flag for touch
+  * @param : none  
+  * @retval : none
+  */
+static void ClearTouch(void)
+{
+  uint8_t i;
+  for(i=0;i<BUTTON_NUM;i++)
+  {
+    Touch->ClearPress(i);
+    Touch->ClearHold(i);
+    Touch->ClearRelease(i);
+  }
+}
+/**
+  * @brief : Test function
+  * @param :   
+  * @retval : 
+  */
+
+
+#ifdef  USE_FULL_ASSERT
+
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t* file, uint32_t line)
+{ 
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+
+  /* Infinite loop */
+  while (1)
+  {
+        if(GPIO_Pin_10==1)
+        {
+          FILE *fp;
+
+            fp = fopen("/cwd/touch_finish", "r");
+        }
+  }
+  while (1)
+  {
+  
+  }
+}
+#endif
