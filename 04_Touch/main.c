d84eb72f13fd3b67e8df85eb71b03b2e75287c1f
congly1956 <congly1956@gmail.com>
2017-03-24 21:07:39 +0700

    Update main.c

--- 04_Touch/main.c
+++ 04_Touch/main.c
@@ -3,13 +3,14 @@
  */
 
 #include <stdio.h>
-
+#include <time.h>
    
 void main()
 {
 	FILE *fp;
   while (1)
   {
+	  sleep 2;
 	 // when GPIO22 set to 1
         if(GPIO_GEN3==1)
         {
@@ -20,7 +21,7 @@ void main()
   }
  while(1)
  {
-	 
+	sleep 2; 
  }
 	
 }
