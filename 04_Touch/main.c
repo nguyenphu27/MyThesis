0c8733decfc96cf732e5e221b31c14b16ed552d7
congly1956 <congly1956@gmail.com>
2017-05-24 23:12:34 +0700

    Update main.c

--- 04_Touch/main.c
+++ 04_Touch/main.c
@@ -17,12 +17,16 @@ int main()
 // bcm2835_set_debug(1);
 	if (!bcm2835_init())
 	return 1;
+	   // Set the pin to be an output
+  //  bcm2835_gpio_fsel(PIN, BCM2835_GPIO_FSEL_OUTP);
+ 
 // Set RPI pin P1-11 to be an input
 	bcm2835_gpio_fsel(PIN, BCM2835_GPIO_FSEL_INPT);	  
 	 FILE *fp;
     while (1)
     {
-	    
+	    // Turn it on
+	//bcm2835_gpio_write(PIN, HIGH);
 	    uint8_t value = bcm2835_gpio_lev(PIN);
 	    // when GPIO17 set to 1
             if(value==1)
