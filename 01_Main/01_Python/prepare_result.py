4e990655d2c05dd3f083bc2dac7d2887f98ef5dc
Huy Duc. Vu <huyvu7495@gmail.com>
2017-06-14 22:52:01 +0700

    new json format

--- 01_Main/01_Python/prepare_result.py
+++ 01_Main/01_Python/prepare_result.py
@@ -5,12 +5,12 @@ Date: Mar 27, 2017
 Update: Mar 27, 2017"""
 import json
 from time import ctime
+from time import time
 def prepare_result():
     "Collecting data from result files and format the data"
     print "Can do chieu cao, can nang, do huyet ap, thu suc keo, ..."
     filestream = list()
     # Can do chieu cao, can nang, do huyet ap, ...
-    
     try:
         filestream.append(open('bluetooth_result','r'))
     except:
@@ -42,18 +42,28 @@ def prepare_result():
         filestream.append('0')
 
     collected=list()
-    collected.append(ctime())
+    mID = int(time())
+    mtime = ctime()
     for item in filestream:
         try:
             collected.append(item.read())
         except AttributeError:
-            collected.append('0')
-        #collected.append(',')
+            collected.append(item)
         try:
             item.close()
         except AttributeError:
             pass
 
     with open("result","a") as result:
-        json.dump(collected[0:len(collected)-1],result)
+        json.dump({'id':mID, 'time':mtime, \
+                    'macid':collected[0],\
+                    'height':collected[1],\
+                    'weight':collected[2],\
+                    'spo2':collected[3],\
+                    'bloodpressure':collected[4],\
+                    'temp':collected[5]\
+                    },result,sort_keys=True)
+        result.write('\n')
         result.close()
+
+prepare_result()
